b83b4cd7ca68635b2bd3e2593c41027c55a73472
Sushant Sharma <ssushants786@gmail.com>
2024-09-22 18:06:58 +0530

    auth-router.js file done

--- backend/router/auth-router.js
+++ backend/router/auth-router.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const router = express.Router(); // express.Router() is a class to handle routes & middlewares
 
+// Below are two methods to define the routes and middlewares
 router.get("/", (req, res) => {
   res.status(200).send("Hello, We are going to learn the MERN Stack!!!...");
 });
+
+// Below is the second method to define the routes, in which we can use chaining of methods:
+router
+  .route("/")
+  .get((req, res) => {
+    res.status(200).send("Hello, We are going to register into platform...");
+  })
+  .post("/", (req, res) => {});
+
+router
+  .route("/register")
+  .get((req, res) => {
+    res.status(200).send("Welcome to the registration page...");
+  })
+  .post("/", (req, res) => {});
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const router = require("./router/auth-router");
-app.use("/api/auth", router); // if we're using it then we don't need to use the below app.get routes.
+// the below code is known as Mounting the router: To use the router in your main express app, you can mount it at a specific URL prefix
+app.use("/api/auth", router);
+// if we're using the above line of code then we don't need to use the below app.get routes.
 // application will use the defined router above and will directly go to the auth-router file on execution
 
 // app.get('/', (req, res) => {
