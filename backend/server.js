ecac02f2705b28861169184fe937202a5f4b356a
Sushant Sharma <ssushants786@gmail.com>
2024-09-23 11:30:18 +0530

    atlas connected

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,12 @@ app.use("/api/auth", router);
 // });
 
 const PORT = process.env.PORT || 5000;
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error(`Failed to connect to the database: ${error.message}`);
   });
-});
--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
-const URI = "mongodb://127.0.0.1:27017/";
+// const URI = "mongodb://127.0.0.1:27017/";
+const URI =
+  "mongodb+srv://ssushants786:bang@123@cluster-codeboss.ijfsb.mongodb.net/cluster-codeboss";
 // Connect to MongoDB
 const connectDB = async () => {
   try {
@@ -7,7 +9,7 @@ const connectDB = async () => {
     console.log("MongoDB connected...");
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
-    process.exit(0);
+    process.exit(1);
   }
 };
 module.exports = connectDB;
