d38deccd3feb51b6f69241a3b0aafa132484f459
Sushant Sharma <ssushants786@gmail.com>
2024-09-24 14:51:48 +0530

    changed .env confign

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const connectDB = require("./utils/db");
 const router = require("./router/auth-router");
-const dotenv = require("dotenv");
-dotenv.config();
 // Middlewares:
 app.use(express.json());
 app.use("/api/auth", router);
