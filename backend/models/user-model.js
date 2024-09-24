65fef5012330812a3f3e81738209d7c1270bd6af
Sushant Sharma <ssushants786@gmail.com>
2024-09-24 14:52:03 +0530

    user schema & model done

---
+++ backend/models/user-model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    }
+}) 
+
+// Define the model or collection name:
+const User = new mongoose.model('User', userSchema);
+module.exports = User;
