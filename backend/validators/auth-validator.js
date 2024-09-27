1343a834c33f02f0ca7f8ebf0b750d0551c9b8ac
Sushant Sharma <ssushants786@gmail.com>
2024-09-27 14:21:38 +0530

    signup schema created for validation

--- backend/validators/auth-validator.js
+++ backend/validators/auth-validator.js
@@ -1,9 +1,41 @@
-const { z } = require('zod');
+// ZOD is middleware and majorly used for registration and login form validation:
+const { z } = require("zod");
 
-// creating an object schema:
+// creating an object schema: This will validate the signup form details entered by the user
 const signupSchema = z.object({
-    username: z.string({required_error: "Name is required"}).min(3).max(20),
-    email: z.string().email(),
-    password: z.string().min(8).max(50),
-    confirmPassword: z.string().ref('password'),
+  username: z
+    .string({ required_error: "Name is required" })
+    .trim()
+    .min(3, { message: "Name must be at least of 3 characters" })
+    .max(50, { message: "Name must not be more than 50 characters" }),
+  email: z
+    .string({ required_error: "Email is required" })
+    .trim()
+    .email({ message: "Please enter a valid email address" })
+    .min(8, { message: "Email must be at least of 3 characters" })
+    .max(20, { message: "Email must not be more than 100 characters" }),
+  password: z
+    .string({ required_error: "Password is required" })
+    .trim()
+    .min(8, { message: "Password must be at least of 8 characters" })
+    .max(20, { message: "Password must not be more than 20 characters" }),
+  confirmPassword: z
+    .string({ required_error: "Confirm password is required" })
+    .trim()
+    .refine(
+      (value) =>
+        value ===
+        z
+          .string()
+          .optional()
+          .refine((confirmPassword) => password === confirmPassword),
+      { message: "Confirm password" }
+    ),
+  phone: z
+    .string({ required_error: "Phone number is required" })
+    .trim()
+    .min(10)
+    .max(15, { message: "Phone number must not be more than 10 digits" }),
 });
+
+module.exports = signupSchema;
