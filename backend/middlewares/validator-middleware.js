500afe20d9e4cb95d5061bef1a4a3404bc583cd6
Sushant Sharma <ssushants786@gmail.com>
2024-10-02 17:18:37 +0530

    error handling improved

---
+++ backend/middlewares/validator-middleware.js
@@ -0,0 +1,23 @@
+// Middleware to validate user input:
+const validateSchema = (schema) => async (req, res, next) => {
+  try {
+    const parseBody = await schema.parseAsync(req.body);
+    req.body = parseBody;
+    next();
+  } catch (err) {
+    const status = 422;
+    const message = "Fill the input properly";
+    const extraDetails = err.errors[0].message;
+
+    const error = {
+      status,
+      message,
+      extraDetails,
+    };
+    // res.status(500).json("Internal Server Error");
+    console.log(error);
+    // res.status(400).json({msg: message});
+    next(error);
+  }
+};
+module.exports = validateSchema;
--- backend/router/auth-router.js
+++ backend/router/auth-router.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router(); // express.Router() is a class to handle routes & middlewares
 const authControllers = require("../controllers/auth-controller");
+const signupSchema = require("../validators/auth-validator");  // using Zod here
+const validateSchema = require("../middlewares/validator-middleware");
+
 // {home, register} likhne ke bajaye ek reference dekar neeche use object ki tarah treat kar skte hain.
 // Below are two methods to define the routes and middlewares
 // router.get("/", (req, res) => {
@@ -10,7 +13,7 @@ const authControllers = require("../controllers/auth-controller");
 // Below is the second method to define the routes, in which we can use chaining of methods:
 router.route("/").get(authControllers.home);
 
-router.route("/register").post(authControllers.register);
+router.route("/register").post(validateSchema(signupSchema), authControllers.register); //1st data will be validated & then a POST reuqest to register route will be sent.
 
 router.route("/login").post(authControllers.login);
 
